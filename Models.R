dae299d4be6a15723182310186814e2787174caa
lmacconn <lmacconn@umich.edu>
2022-09-08 23:48:19 -0400

    Foor loops added with Kerby

--- Models.R
+++ Models.R
@@ -1,10 +1,25 @@
 
 regression <- function(Y, X, A){
-  svyglm(paste(Y, "~", X, "+", A), design=dfw, data=df)
+ m = svyglm(paste(Y, "~", X, "+", A), design=dfw, data=df)
+ s= summary(m)
+ return(s)
 }
+mlist = NULL
+
+for (Y in c("logIL6", "logTNF1", "logCRP")) {
+  for (X in c("prison", "combatzone", "homeless")) {
+    m = regression(Y, prison, age)
+    mlist = c(mlist, m)  
+  }
+}
+
+
+print(mlist)
 regression(logIL6, prison, age)
 load()
 svyglm(logIL6~prison+age, design = dfw, data=df)
 summary( svyglm(logIL6~prison+age, design = dfw, data=df) )
 svyglm(logIL6~prison+age, design =dfw)
 summary(svyglm(logIL6~prison+age, design =dfw))
+summary(lm(logIL6~prison+age, data=df, weights = PVBSWGTR))
+
